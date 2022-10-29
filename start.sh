ce8611935c33c943390586a4927c75cddb3df76b
alfin fernandha <56243349+alfin-nandha@users.noreply.github.com>
2022-10-29 19:24:47 +0700

    change prod port

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 sudo docker build -t joshgarden-app:latest .
-sudo docker run -d -p 80:8080 --env-file ./.env --name joshgarden joshgarden-app:latest
+sudo docker run -d -p 8080:8080 --name joshgarden joshgarden-app:latest
