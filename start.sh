8efa7874f193304573a13e392feb527c300b55dd
alfin fernandha <56243349+alfin-nandha@users.noreply.github.com>
2022-10-29 19:00:27 +0700

    adit port

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 sudo docker build -t joshgarden-app:latest .
-sudo docker run -d -p 80:8000 --name joshgarden joshgarden-app:latest
+sudo docker run -d -p 80:8080 --name joshgarden joshgarden-app:latest
